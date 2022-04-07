71a9424095ffb1c065c2f5b835c513d92cad366b
Guilherme Pires <guilherme.pires@alleninstitute.org>
2022-04-07 07:52:12 -0700

    gripp logo

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,30 +3,22 @@
 </script>
 
 <script lang="ts">
-	import Counter from '$lib/Counter.svelte';
 </script>
 
 <svelte:head>
-	<title>Home</title>
+	<title>gripp.engineering - get one.</title>
 </svelte:head>
 
 <section>
 	<h1>
 		<div class="welcome">
 			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
+			<img src="gripp.svg">	
 			</picture>
 		</div>
 
-		to your new<br />SvelteKit app
 	</h1>
 
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-
-	<Counter />
 </section>
 
 <style>
@@ -42,14 +34,14 @@
 		width: 100%;
 	}
 
-	.welcome {
+	.logo {
 		position: relative;
 		width: 100%;
 		height: 0;
 		padding: 0 0 calc(100% * 495 / 2048) 0;
 	}
 
-	.welcome img {
+	.logo img {
 		position: absolute;
 		width: 100%;
 		height: 100%;
